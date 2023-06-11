66fd80743f504e308a1d8d908a3695266d79dbb9
Lê Phước Thông Ân <45901888+thongan@users.noreply.github.com>
2023-06-11 21:05:45 +0700

    Add files via upload
    Add file manually without using git commit because of error of system

---
+++ WhileBreaker.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+counter=0
+
+while [ $counter -lt 10 ]
+do
+  # Increase $counter by 1
+  ((counter++))
+  echo "Counter: $counter"
+
+  if [ $counter == 2 ]
+  then
+    continue
+  elif [ $counter == 4 ]
+  then
+    break
+  fi
+done
